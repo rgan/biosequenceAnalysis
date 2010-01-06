451d63319906ada92c85d4a50364df98303fe82e
rgan <rajaram.ganeshan@gmail.com>
2010-01-06 14:49:35 -0500

    Added Smith-Waterman local alignment algorithm

--- pairwise-alignment/src/SmithWatermanLocalAlignment.scala
+++ pairwise-alignment/src/SmithWatermanLocalAlignment.scala
@@ -2,56 +2,53 @@ package org.biosequenceanalysis
 
 import BackPointer._
 
-class NeedlemanWunschGlobalAlignment(sequence1 : List[String], sequence2 : List[String], substitutionMatrix: Matrix, gapPenalty : Int) {
+class SmithWatermanLocalAlignment(sequence1 : List[String], sequence2 : List[String], substitutionMatrix: Matrix, gapPenalty : Int) {
   val this.sequence1 = sequence1
   val this.sequence2 = sequence2
   val this.gapPenalty = gapPenalty
 
+  // all entries in the matrix are initialized to 0
   var matrix = new Matrix("X" :: this.sequence1, "X" :: this.sequence2)
   var backPointers = new Array[Array[BackPointer]](matrix.noRows,matrix.noCols)
-  
-  // initialize top row F(0,j) to -i*gapPenalty
-  for(j <- 0 to matrix.noCols-1) {
-      matrix.setValueAt(0, j, -1*j*gapPenalty)
-      backPointers(0)(j) = Left
-  }
-  // initialize left column F(i, 0) to -j*gapPenalty
-  for(i <- 0 to matrix.noRows-1) {
-      matrix.setValueAt(i, 0, -1*i*gapPenalty)
-      backPointers(i)(0) = Top
-  }
 
   // computes the score with backpointer for a given i,j
   def score(row : Int, col: Int) : Tuple2[Int, BackPointer] = {
-    if (row == 0) return (matrix.getValueAt(row, col), Left)
-    if (col == 0) return (matrix.getValueAt(row, col), Top)
+    if (row == 0) return (matrix.getValueAt(row, col), None)
+    if (col == 0) return (matrix.getValueAt(row, col), None)
     val diagonal: Int = matrix.getValueAt(row - 1, col - 1) + substitutionMatrix.valueFor(sequence1(row - 1), sequence2(col - 1))
     val left: Int = matrix.getValueAt(row, col - 1) - gapPenalty
     val top: Int = matrix.getValueAt(row - 1, col) - gapPenalty
-    val tuple = Utils.max(diagonal :: left :: (top :: Nil), Utils.MIN_VALUE, 0, 0)
+    val tuple = Utils.max(diagonal :: left :: top :: (0 :: Nil), Utils.MIN_VALUE, 0, 0)
     return (tuple._1, tuple._2 match {
       case 0 => Diagonal
       case 1 => Left
       case 2 => Top
+      case 3 => None
     })
   }
 
   // returns optimal alignment found along with the score
   def find() : Tuple2[Int, Tuple2[List[String], List[String]]] = {
+     var maxScore = Utils.MIN_VALUE
+     var maxScoreLocation: Tuple2[Int, Int] = (-1,-1)
      for(i <- 1 to matrix.noRows-1) {
         for(j <- 1 to matrix.noCols-1) {
           val scoreWithBackPointer = score(i, j)
           matrix.setValueAt(i, j, scoreWithBackPointer._1)
           backPointers(i)(j) = scoreWithBackPointer._2
+          if (scoreWithBackPointer._1 > maxScore) {
+             maxScore = scoreWithBackPointer._1
+             maxScoreLocation = (i, j)
+          }
       }
-     } 
-     return (matrix.getValueAt(matrix.noRows-1, matrix.noCols-1),
-             traceback(matrix.noRows-1, matrix.noCols-1, Nil, Nil))
+     }
+     return (maxScore,
+             traceback(maxScoreLocation._1, maxScoreLocation._2, Nil, Nil))
   }
 
   // returns optimal alignment using the stored backpointers
   def traceback(i : Int, j : Int, seq1 : List[String], seq2 :List[String]) : Tuple2[List[String], List[String]] =  {
-    if (i == 0 && j == 0) {
+    if (matrix.getValueAt(i,j) == 0) {
       return (seq1, seq2)
     }
     backPointers(i)(j) match {
@@ -60,4 +57,5 @@ class NeedlemanWunschGlobalAlignment(sequence1 : List[String], sequence2 : List[
       case Top => traceback(i-1, j, matrix.rowSequenceValueAt(i) :: seq1, "-" :: seq2 )
     }
   }
+  
 }
